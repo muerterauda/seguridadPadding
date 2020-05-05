5c3a3d6ce6f49430a1ac7aa70a60038007a95f50
juanpalmaborda@uma.es <juanpalmaborda@hotmail.com>
2020-05-05 11:29:37 +0200

    Fin

--- src/ClienteOracle.java
+++ src/ClienteOracle.java
@@ -1,23 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.CookieHandler;
-import java.net.CookieManager;
-import java.net.CookiePolicy;
-import java.net.URL;
+import java.net.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  *
- * @author isaac
+ * @author Juan Palma Borda
+ * @author Isaac
  */
 public class ClienteOracle {
 
