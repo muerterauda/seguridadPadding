7a7d683eed714c47e696681faf8f56d52df771dc
Muerterauda <juanpalmaborda@hotmail.com>
2020-05-13 17:18:44 +0200

    Comentarios en el codigo

--- src/ClienteOracle.java
+++ src/ClienteOracle.java
@@ -93,21 +93,33 @@ public class ClienteOracle {
         return sb.toString();
     }
 
+    /**
+     * Metodo que te devuelve la String resultante del proceso de descifrado llevado a cabo
+     * por el oraculo de padding gracias al disinto tipo de error que provoca el padding
+     * @param e Cadena hexadecimal a la que romper la seguridad
+     * @throws IOException
+     */
    public static void getError(String e) throws IOException {
+       //Se consigue el IV original
        String iv_ori =e.substring(0, 32);
        System.out.println(iv_ori);
        String[] bloques32 = new String[4];
+       //Se Separa la cadena en los bloques
        for (int c=32;c<e.length();c+=32)
        {
            bloques32[(c/32)-1] = e.substring(c, c+32);
        }
+       //Se muestran los distintos bloques
        pintarArray(bloques32);
        List<List<String>> solucion_completa = new ArrayList<>();
+       // Se realiza el algoritmo para cada bloque, para los que se buscan las soluciones a los disitnas parejas hexadecimales
        for(String bloque: bloques32){
            String iv = "00000000000000000000000000000000";
            List<String> sol = new ArrayList<>();
            String pad = "01";
+           // Se requieren 16 soluciones para cada bloque, ya que son 16 parejas a resolver
            while(sol.size()<16){
+               //Se parte el IV en 3, lo que falta, la pareja que se esta resolviendo y lo que se tiene resuelto y al que se le aplica el padding correspondiente
                String sol_hex= "00";
                String iv_sol = cacularIVNuevo(sol, pad);
                iv = iv.substring(0, iv.length()-2);
@@ -120,6 +132,7 @@ public class ClienteOracle {
                        sol_hex = "0"+sol_hex;
                    }
                }
+               //Se imprime la solucion de la pareja por motivos de control y se añade a las soluciones
                System.out.println(sol_hex);
                sol.add(0, xor(pad, sol_hex));
                pad = dameSiguiente(pad);
@@ -127,9 +140,11 @@ public class ClienteOracle {
                    pad = "0"+pad;
                }
            }
+           // Se muestra la solucion del bloque por motivos de control y se añade a las solucion completa
            System.out.println(sol);
            solucion_completa.add(sol);
        }
+       // Se muestran todas las soluciones y se procede a aplicar el xor final
        printSolucion(solucion_completa);
        String[] iv_com = {iv_ori, bloques32[0], bloques32[1], bloques32[2]};
        System.out.println("\n-----------------------------------------------");
@@ -140,7 +155,14 @@ public class ClienteOracle {
        }
        System.out.println("\n-----------------------------------------------");
    }
+    /**
+     * Metodo que te devuelve la String resultante del proceso de descifrado llevado a cabo
+     * por el oraculo de padding gracias al tiempo estimatorio que tarda al responderte la pagian con distintos tipos de padding
+     * @param e Cadena hexadecimal a la que romper la seguridad
+     * @throws IOException
+     */
     public static void getTime(String e) throws IOException {
+        //Se consigue el IV original
         String iv_ori =e.substring(0, 32);
         System.out.println(iv_ori);
         String[] bloques32 = new String[4];
@@ -148,16 +170,21 @@ public class ClienteOracle {
         {
             bloques32[(c/32)-1] = e.substring(c, c+32);
         }
+        //Se muestran los distintos bloques
         pintarArray(bloques32);
         List<List<String>> solucion_completa = new ArrayList<>();
+        // Se realiza el algoritmo para cada bloque, para los que se buscan las soluciones a los disitnas parejas hexadecimales
         for(String bloque: bloques32){
             String iv = "00000000000000000000000000000000";
             List<String> sol = new ArrayList<>();
             String pad = "01";
+            // Se requieren 16 soluciones para cada bloque, ya que son 16 parejas a resolver
             while(sol.size()<16){
+                //Se parte el IV en 3, lo que falta, la pareja que se esta resolviendo y lo que se tiene resuelto y al que se le aplica el padding correspondiente
                 String sol_hex= "00";
                 String iv_sol = cacularIVNuevo(sol, pad);
                 iv = iv.substring(0, iv.length()-2);
+                // La estimacion esta puesta para mas de 200, ya que los valores incorrectos no superan los 100 y los valores corrector suelen rondar los 400 y pico
                 while(timingOracle(iv+sol_hex+iv_sol+bloque)<200){
                     if(sol_hex.equals("ff")||sol_hex.equals("FF")){
                         throw new RuntimeException("Problema encontrado");
@@ -167,6 +194,7 @@ public class ClienteOracle {
                         sol_hex = "0"+sol_hex;
                     }
                 }
+                //Se imprime la solucion de la pareja por motivos de control y se añade a las soluciones
                 System.out.println(sol_hex);
                 sol.add(0, xor(pad, sol_hex));
                 pad = dameSiguiente(pad);
@@ -174,9 +202,11 @@ public class ClienteOracle {
                     pad = "0"+pad;
                 }
             }
+            // Se muestra la solucion del bloque por motivos de control y se añade a las solucion completa
             System.out.println(sol);
             solucion_completa.add(sol);
         }
+        // Se muestran todas las soluciones y se procede a aplicar el xor final
         printSolucion(solucion_completa);
         String[] iv_com = {iv_ori, bloques32[0], bloques32[1], bloques32[2]};
         System.out.println("\n-----------------------------------------------");
@@ -188,7 +218,15 @@ public class ClienteOracle {
         System.out.println("\n-----------------------------------------------");
     }
 
+    /**
+     * Metodo que te devuelve la String resultante del proceso de descifrado llevado a cabo
+     * por el oraculo de padding gracias al tiempo que tarda al responderte la pagian con distintos tipos de padding,
+     * esta version difiere de la anterior en que en esta se consigue el maximo tiempo.
+     * @param e Cadena hexadecimal a la que romper la seguridad
+     * @throws IOException
+     */
     public static void getTimeExacto(String e) throws IOException {
+        //Se consigue el IV original
         String iv_ori = e.substring(0, 32);
         System.out.println(iv_ori);
         String[] bloques32 = new String[4];
@@ -196,19 +234,23 @@ public class ClienteOracle {
         {
             bloques32[(c/32)-1] = e.substring(c, c+32);
         }
+        //Se muestran los distintos bloques
         pintarArray(bloques32);
         List<List<String>> solucion_completa = new ArrayList<>();
         for(String bloque: bloques32){
             String iv = "00000000000000000000000000000000";
             List<String> sol = new ArrayList<>();
             String pad = "01";
+            // Se requieren 16 soluciones para cada bloque, ya que son 16 parejas a resolver
             while(sol.size()<16){
+                //Se parte el IV en 3, lo que falta, la pareja que se esta resolviendo y lo que se tiene resuelto y al que se le aplica el padding correspondiente
                 String sol_hex= "00";
                 String hex_sol = "00";
                 String iv_sol = cacularIVNuevo(sol, pad);
                 iv = iv.substring(0, iv.length()-2);
                 timingOracle(iv+sol_hex+iv_sol+bloque);
                 long time =  timingOracle(iv+sol_hex+iv_sol+bloque);
+                // No se realiza estimacion puesto que se consiguen todos los resultados y se asigna como correcto al mas alto
                 while(!sol_hex.equals("ff")){
                     sol_hex = dameSiguiente(sol_hex);
                     if(sol_hex.length()<2){
@@ -220,6 +262,7 @@ public class ClienteOracle {
                         time = timeHex;
                     }
                 }
+                //Se imprime la solucion de la pareja y el tiempo que ha tardado por motivos de control, si ha tardado mucho (mas de 550) vigilar porque probablemente haya habido algun problema en la redd y se añade a las soluciones
                 System.out.println(hex_sol+": "+ time);
                 sol.add(0, xor(pad, hex_sol));
                 pad = dameSiguiente(pad);
@@ -227,9 +270,11 @@ public class ClienteOracle {
                     pad = "0"+pad;
                 }
             }
+            // Se muestra la solucion del bloque por motivos de control y se añade a las solucion completa
             System.out.println(sol);
             solucion_completa.add(sol);
         }
+        // Se muestran todas las soluciones y se procede a aplicar el xor final
         printSolucion(solucion_completa);
         String[] iv_com = {iv_ori, bloques32[0], bloques32[1], bloques32[2]};
         System.out.println("\n-----------------------------------------------");
@@ -253,8 +298,7 @@ public class ClienteOracle {
         //simplemente cargará la cookie necesaria para las demás llamadas
         login("JuanPalmaBorda");
         //Es la cadena que queremos descifrar usando el oraculo de padding.
-        String e = "c0ad3d84be178fbd3475f3b5271d0ee9cbc1d28c9197805a933cc67180703e1826b4ad6fc6376b976f05053679924e2cc6550c6c59686e0e70193fa038b587a5917a1a082251135b2e2a2b49a0120c85";
-        String e1 = "50c0acfb539f120c905ffc2ba2a26c4f94be2250449e707249e306edb69b51223bc01e8734e1b2d970fc534c93a619d99de233383fe9a155f172882d415a15c792238d1a231a716b2eabfe7efd1e3539";
+        String e = "50c0acfb539f120c905ffc2ba2a26c4f94be2250449e707249e306edb69b51223bc01e8734e1b2d970fc534c93a619d99de233383fe9a155f172882d415a15c792238d1a231a716b2eabfe7efd1e3539";
         String t = "32d5454a21ba5850cb19d6cb5f5b89c477d4892b1443271e601e5d9264f33ce89e7f46ad7ea842d3c2fd477aef53a20b5509aa655a55d2d778fcaf07c88f3bf1bb00ff438108dcd3414c76be47ded71b";
         getTime(t);
 
